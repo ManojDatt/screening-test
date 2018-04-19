4210a095649767535431946b0cb83eb2c5f056d4
ManojDatt <manojdatt1it@gmail.com>
2018-04-19 07:11:50 -0500

    middelware

---
+++ app/middelware/ip_monitor_middleware.rb
@@ -0,0 +1,37 @@
+module ActionDispatch
+  class Request < Rack::Request
+    def ip
+      @ip ||= super
+    end
+
+    def self.remote_ip(env)
+      @env = env  
+      @remote_ip ||= (@env["action_dispatch.remote_ip"] || ip).to_s
+    end
+  end
+end
+
+class IpMonitorMiddleware
+    include ActionDispatch
+
+	def initialize app
+	    @app = app
+	end
+
+	def call env
+    request_started_on = Time.now
+    @status, @headers, @response = @app.call(env)
+    request_ended_on = Time.now
+
+    Rails.logger.debug "=" * 50
+    Rails.logger.debug "Request delta time: #{request_ended_on - request_started_on} seconds."
+    Rails.logger.debug "=" * 50
+    Rails.logger.debug "#" * 50
+    Rails.logger.debug "Request IP : #{Request.remote_ip(env)}"
+    Rails.logger.debug "#" * 50
+
+    [@status, @headers, @response]
+  end
+end
+
+
